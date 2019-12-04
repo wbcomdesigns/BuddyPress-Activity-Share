3f1812a6dd78dbd10cdff16269dc491e93af1635
Pallavi Gupta <pallavi@wbcomdesigns.com>
2019-12-04 18:07:27 +0530

    (#44) Added theme listing using json file.

--- admin/wbcom/templates/wbcom-themes-page.php
+++ admin/wbcom/templates/wbcom-themes-page.php
@@ -1,104 +1,76 @@
 <div class="wrap">
 	<?php echo do_shortcode( '[wbcom_admin_setting_header]' ); ?>
 	<div class="reign-demos-wrapper">
-		<h4 class="wbcom-demo-name"><?php esc_html_e( 'Reign BuddyPress', 'buddypress-share' ); ?></h4>
-		<div class="wbcom-demo-content-wrap">
-			<div class="wbcom-demo-importer">
-				<div class="container">
-					<div class="wbcom-image-wrapper">
-						<img src="<?php echo BP_ACTIVITY_SHARE_PLUGIN_URL . 'admin/wbcom/assets/imgs/reign.jpg'; ?>" alt="Avatar" class="image" style="width:100%">
-					</div>
-					<div class="wbcom-demo-title">
-						<h2><?php esc_html_e( 'REIGN Community', 'buddypress-share' ); ?></h2>
-						<ul class="wbcom_theme_features_list">
-                            <li><?php esc_html_e( 'Multiple Member Directory Layout', 'buddypress-share' ); ?></li>
-                            <li><?php esc_html_e( 'Multiple Member Header Layout', 'buddypress-share' ); ?></li>
-                            <li><?php esc_html_e( 'Multiple Group Directory Layout', 'buddypress-share' ); ?></li>
-                            <li><?php esc_html_e( 'Multiple Group Header Layout', 'buddypress-share' ); ?></li>
-                            <li><?php esc_html_e( 'BuddyPress Customization Options', 'buddypress-share' ); ?></li>
-                            <li><?php esc_html_e( 'BuddyPress Extra Widgets', 'buddypress-share' ); ?></li>
-                            <li><?php esc_html_e( 'Inbuilt Social Profile Module', 'buddypress-share' ); ?></li>
-                        </ul>
-						<div class="wbcom-middle">
-							<a href="https://wbcomdesigns.com/downloads/reign-buddypress-theme/#pricing-table-leranmate" class="wbcom-button wbcom-purchase" target="_blank"><?php esc_html_e( 'Purchase', 'buddypress-share' ); ?></a>
-							<a target="_blank" href="http://demos.wbcomdesigns.com/reign-demo1/" class="wbcom-button"><?php esc_html_e( 'Preview', 'buddypress-share' ); ?></a>
+			<?php
+			$wb_json_request_url           = WB_PLUGINS_JSON_URL . 'bp-activity-social-share/themes.json';
+    		$theme_request_url             = BP_ACTIVITY_SHARE_PLUGIN_URL . 'admin/wbcom/assets/json/themes.json';
+
+			$response_wb_json_request      = wp_remote_get( $wb_json_request_url, array( 'timeout' => 120 ) );
+			$response_wb_json_request_head = wp_remote_head( $wb_json_request_url );
+			$response_themes               = array();
+			$get_plugin_response           = false;
+
+			if ( ! is_wp_error( $response_wb_json_request_head ) ) {
+
+				if ( isset( $response_wb_json_request_head['response']['code'] ) && ( $response_wb_json_request_head['response']['code'] == 200 ) ) {
+					$response_themes = isset( $response_wb_json_request['body'] ) ? $response_wb_json_request['body'] : '';
+				}
+			}
+
+			if ( empty( $response_themes ) ) {
+				$response_theme_request  = wp_remote_get( $theme_request_url, array( 'timeout' => 120 ) );
+				if ( ! is_wp_error( $response_theme_request ) ) {
+					if ( isset( $response_theme_request['response']['code'] ) && ( $response_theme_request['response']['code'] == 200 ) ) {
+						$response_themes = isset( $response_theme_request['body'] ) ? $response_theme_request['body'] : '';
+						$get_plugin_response = true;
+					}
+				}
+			}
+
+			if ( ! empty( $response_themes ) ) {
+				$response_themes = json_decode( $response_themes, true );
+				if( !empty( $response_themes ) && is_array( $response_themes ) ) {
+					$wb_admin_obj = new Wbcom_Admin_Settings();
+					foreach ( $response_themes as $key => $theme_details ) {
+						if ( $get_plugin_response ) {
+							$image_url = BP_ACTIVITY_SHARE_PLUGIN_URL . 'admin/wbcom/assets/imgs/'. $theme_details['image_url'];
+						} else {
+							$image_url = $theme_details['image_url'];
+						}
+						?>
+						<h4 class="wbcom-demo-name">
+							<?php echo $key; ?>
+						</h4>
+						<div class="wbcom-demo-content-wrap">
+							<div class="wbcom-demo-importer">			
+								<div class="container">
+									<div class="wbcom-image-wrapper">
+										<img src="<?php echo $image_url; ?>" alt="Image" class="image" style="width:100%">
+									</div>
+									<div class="wbcom-demo-title">
+										<h2><?php echo $theme_details['name']; ?></h2>
+										<ul class="wbcom_theme_features_list">
+											<?php
+											if ( ! empty( $theme_details['features'] ) ) {
+												foreach( $theme_details['features'] as $single_feature ) { ?>
+				                            	<li><?php echo $single_feature; ?></li>
+				                            	<?php }
+				                        	}
+				                            ?>
+				                        </ul>
+										<div class="wbcom-middle">
+											<a href="<?php echo $theme_details['purchase_url']; ?>" class="wbcom-button wbcom-purchase" target="_blank"><?php esc_html_e( 'Purchase', 'buddypress-share' ); ?></a>
+											<a target="_blank" href="<?php echo $theme_details['demo_url']; ?>" class="wbcom-button"><?php esc_html_e( 'Preview
+											', 'buddypress-share' ); ?></a>
+										</div>
+									</div>
+								</div>
+							</div>
 						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-		<h4 class="wbcom-demo-name"><?php esc_html_e( 'LearnMate', 'buddypress-share' ); ?></h4>
-		<div class="wbcom-demo-content-wrap">
-			<div class="wbcom-demo-importer">
-				<div class="container">
-					<div class="wbcom-image-wrapper">
-						<img src="<?php echo BP_ACTIVITY_SHARE_PLUGIN_URL . 'admin/wbcom/assets/imgs/learnmate.jpg'; ?>" alt="Avatar" class="image" style="width:100%">
-					</div>
-					<div class="wbcom-demo-title">
-						<h2><?php esc_html_e( 'Learnmate', 'buddypress-share' ); ?></h2>
-						<ul class="wbcom_theme_features_list">
-                            <li><?php esc_html_e( 'Add Review to your course', 'buddypress-share' ); ?></li>
-                            <li><?php esc_html_e( 'Course Coming Soon', 'buddypress-share' ); ?></li>
-                            <li><?php esc_html_e( 'Grid And List Layout', 'buddypress-share' ); ?></li>
-                            <li><?php esc_html_e( 'Learndash/LifterLMS Extra Widgets', 'buddypress-share' ); ?></li>
-                            <li><?php esc_html_e( 'Unparalleled Course Layout', 'buddypress-share' ); ?></li>
-                            <li><?php esc_html_e( 'Awesome Learndash/LifterLMS Pages', 'buddypress-share' ); ?></li>
-                            <li><?php esc_html_e( 'Distraction Free Reading', 'buddypress-share' ); ?></li>
-                        </ul>
-						<div class="wbcom-middle">
-							<a href="https://wbcomdesigns.com/downloads/learnmate-learndash/#pricing-table-leranmate" class="wbcom-button wbcom-purchase" target="_blank"><?php esc_html_e( 'Purchase', 'buddypress-share' ); ?></a>
-							<a target="_blank" href="https://wpbp.in/learndash/" class="wbcom-button" ><?php esc_html_e( 'Preview', 'buddypress-share' ); ?></a>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-		<h4 class="wbcom-demo-name"><?php esc_html_e( 'StoreMate', 'buddypress-share' ); ?></h4>
-		<div class="wbcom-demo-content-wrap">
-			<div class="wbcom-demo-importer">
-				<div class="container">
-					<div class="wbcom-image-wrapper">
-						<img src="<?php echo BP_ACTIVITY_SHARE_PLUGIN_URL . 'admin/wbcom/assets/imgs/storemate.jpg'; ?>" alt="Avatar" class="image" style="width:100%">
-					</div>
-					<div class="wbcom-demo-title">
-						<h2><?php esc_html_e( 'ShopMate Dokan', 'buddypress-share' ); ?></h2>
-						<ul class="wbcom_theme_features_list">
-                            <li><?php esc_html_e( 'Dokan Extra Widgets', 'buddypress-share' ); ?></li>
-                            <li><?php esc_html_e( 'Unique Single Product Page Customization', 'buddypress-share' ); ?></li>
-                            <li><?php esc_html_e( 'Awesome WooCommerce Pages', 'buddypress-share' ); ?></li>
-                            <li><?php esc_html_e( 'Grid And List Layout', 'buddypress-share' ); ?></li>
-                            <li><?php esc_html_e( 'Automated Mega Menu Support', 'buddypress-share' ); ?></li>
-                        </ul>
-						<div class="wbcom-middle">
-							<a target="_blank" href="https://wbcomdesigns.com/downloads/storemate-dokan/#pricing-table-leranmate" class="wbcom-button wbcom-purchase"><?php esc_html_e( 'Purchase', 'buddypress-share' ); ?></a>
-							<a target="_blank" href="https://demos.wbcomdesigns.in/reign-dokan/" class="wbcom-button"><?php esc_html_e( 'Preview', 'buddypress-share' ); ?></a>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-		<h4 class="wbcom-demo-name"><?php esc_html_e( 'Jobmate', 'buddypress-share' ); ?></h4>
-		<div class="wbcom-demo-content-wrap">
-			<div class="wbcom-demo-importer">
-				<div class="container">
-					<div class="wbcom-image-wrapper">
-						<img src="<?php echo BP_ACTIVITY_SHARE_PLUGIN_URL . 'admin/wbcom/assets/imgs/jobmate.jpg'; ?>" alt="Avatar" class="image" style="width:100%">
-					</div>
-					<div class="wbcom-demo-title">
-						<h2><?php esc_html_e( 'Jobmate Theme', 'buddypress-share' ); ?></h2>
-						<ul class="wbcom_theme_features_list">
-                            <li><?php esc_html_e( 'Simple live searching and filtering', 'buddypress-share' ); ?></li>
-                            <li><?php esc_html_e( 'Unique Job listing Page', 'buddypress-share' ); ?></li>
-                            <li><?php esc_html_e( 'Awesome Resume list Page', 'buddypress-share' ); ?></li>
-                            <li><?php esc_html_e( 'Outstanding all job maanger pagesLayout', 'buddypress-share' ); ?></li>
-                        </ul>
-						<div class="wbcom-middle">
-							<a target="_blank" href="https://wbcomdesigns.com/downloads/jobmate-theme/" class="wbcom-button wbcom-purchase"><?php esc_html_e( 'Purchase', 'buddypress-share' ); ?></a>
-							<a target="_blank" href="http://demos.wbcomdesigns.in/jobmate-theme/" class="wbcom-button"><?php esc_html_e( 'Preview', 'buddypress-share' ); ?></a>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
+						<?php
+					}
+				}
+			}			
+			?>
 	</div>
 </div>
