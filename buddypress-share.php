7fc1be250b908d670015aead455c24c51e9e6514
Varun Kumar Dubey <vapvarun@gmail.com>
2020-05-11 01:18:33 +0530

    Update buddypress-share.php

--- buddypress-share.php
+++ buddypress-share.php
@@ -14,7 +14,7 @@
  * Plugin Name:       BuddyPress Activity Social Share
  * Plugin URI:        https://www.wbcomdesigns.com
  * Description:       This plugin will add an extended feature to the big name “BuddyPress” that will allow to share Activity “Post Updates” to the social sites.
- * Version:           2.4.0
+ * Version:           2.5.0
  * Author:            Wbcom Designs<admin@wbcomdesigns.com>
  * Author URI:        https://www.wbcomdesigns.com
  * License:           GPL-2.0+
